e46eeb3d03e6cd806651f8652b40e5a1f0ff720d
Geidel Guerra <github.6870z@passmail.net>
2024-08-20 21:43:00 -0400

    wip

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ void Example1() {
         DrawCircle(x, y, 5, WHITE);
         angle += increment;
     }
+
+    DrawText("Move the mouse", 22, GetScreenHeight() - 42, 20, GRAY);
 }
 
 void Example2() {
@@ -35,18 +37,44 @@ void Example2() {
     char text[20];
     sprintf(text, "%0.1f°", angle * RAD2DEG);
     DrawText(text, center.x + 20, center.y - 20, 20, GRAY);
+
+    DrawText("Move the mouse", 22, GetScreenHeight() - 42, 20, GRAY);
 }
 
 int main() {
     InitWindow(800, 800, "Trigonometry");
     SetTargetFPS(144);
+    int activeExample = 1;
+    int examplesCount = 2;
 
     while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_ONE)) {
+            activeExample = 1;
+        }
+
+        if (IsKeyPressed(KEY_TWO)) {
+            activeExample = 2;
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
 
-        // Example1();
-        Example2();
+        switch (activeExample) {
+            case 1: Example1();
+            break;
+            case 2: Example2();
+            break;
+        }
+
+        float textX = 22;
+        for (int i = 1; i <= examplesCount; i++) {
+            char text[10];
+            sprintf(text, "%d", i);
+            int length = MeasureText(text, 20);
+            Color color = i == activeExample ? WHITE : GRAY;
+            DrawText(text, textX, 22, 20, color);
+            textX += length + 5;
+        }
 
         EndDrawing();
     }
